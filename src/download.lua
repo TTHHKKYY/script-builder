3c103b61488298d57235defdda3529cc01616cc0
TTHHKKYY <THKY2@protonmail.com>
2022-01-12 18:52:08 -0700

    Add Commands, Remove Interface

--- src/download.lua
+++ src/download.lua
@@ -3,61 +3,63 @@ local Http = game:GetService("HttpService")
 print(Http:GetAsync("https://api.github.com/zen"))
 
 local LocalPlayer = owner
-
--- destroy the last interface, if there was one.
-local LastInterface = LocalPlayer.PlayerGui:FindFirstChild("RepositoryIndex") do
-	if LastInterface then
-		LastInterface:Destroy()
-	end
-end
-
-local Interface = Instance.new("ScreenGui")
-local Container = Instance.new("ScrollingFrame")
-local List = Instance.new("UIListLayout")
-
-local Button = Instance.new("TextButton")
-
----- interface
-
-List.Parent = Container
-
-Button.TextSize = 25
-Button.BackgroundTransparency = 1
-Button.TextColor3 = Color3.new(0,0,0)
-Button.Size = UDim2.new(1,0,0,30)
-Button.TextXAlignment = Enum.TextXAlignment.Left
-
-Container.BackgroundTransparency = 0.8
-Container.BackgroundColor3 = BrickColor.new("White").Color
-Container.BorderColor3 = BrickColor.new("Black").Color
-Container.ScrollBarImageColor3 = BrickColor.new("Black").Color
-
-Container.CanvasSize = UDim2.new()
-Container.Size = UDim2.new(0,300,0,600)
-Container.Position = UDim2.new(1,-380,1,-610)
-Container.AutomaticCanvasSize = Enum.AutomaticSize.Y
-Container.Parent = Interface
-
-Interface.ResetOnSpawn = false
-Interface.Name = "RepositoryIndex"
-Interface.Parent = LocalPlayer.PlayerGui
-
-local function ClearInterface()
-	for _,Item in pairs(Container:GetChildren()) do
-		if Item ~= List then
-			Item:Destroy()
-		end
-	end
-end
-
-----
+local Owner = ""
+local Repo = ""
 
 LocalPlayer.Chatted:Connect(function(Message)
 	local Arguments = string.split(Message," ")
 	
-	if string.find(Arguments[1],"^/") then
-		local Command = Arguments[1]
+	local Command = Arguments[1]
+	local Value = Arguments[2]
+	
+	-- /help is already in use by the default chat scripts
+	if Command == "/githelp" then
+		print("/user NAME")
+		print("/repo NAME")
+		print("/index [PATH]")
+		print("/loadpath PATH")
 		
-		loadstring(Http:GetAsync(Arguments[2]))
+		print("Current user: " .. Owner)
+		print("Current repository: " .. Repo)
+	end
+	
+	if Command == "/user" then
+		assert(Value,"Missing username.")
+		print("Set user to " .. Value)
+		
+		Owner = Value
+	end
+	if Command == "/repo" then
+		assert(Value,"Missing repository name.")
+		print("Set repository to " .. Value)
+		
+		Repo = Value
+	end
+	
+	if Command == "/index" then
+		assert(Owner ~= "","User is not set.")
+		assert(Repo ~= "","Repository is not set.")
+		
+		local Index = Http:GetAsync(string.format("https://api.github.com/repos/%s/%s/contents/%s",Owner,Repo,Value or "/"))
+		local IndexData = Http:JSONDecode(Index)
+		
+		for _,File in pairs(IndexData) do
+			print("/" .. File["path"])
+		end
+	end
+	
+	if Command == "/loadpath" then
+		assert(Owner ~= "","User is not set.")
+		assert(Repo ~= "","Repository is not set.")
+		assert(Value,"Path is missing.")
+		
+		local Data = Http:GetAsync(string.format("https://raw.githubusercontent.com/%s/%s/%s",Owner,Repo,Value))
+		local Compiled,Error = loadstring(Data)
+		
+		if Compiled then
+			Compiled()
+		else
+			error(Error)
+		end
 	end
 end)
