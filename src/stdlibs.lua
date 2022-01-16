31e86684e7b697ba55868a1daa3736ffcaf8a443
lego man <12103838+rwilliaise@users.noreply.github.com>
2022-01-15 21:46:59 -0800

    fix: update libs to avoid cyclic dependency issues

--- src/stdlibs.lua
+++ src/stdlibs.lua
@@ -15,27 +15,10 @@ local require = function(module)
     local foundScript = __scripts[module]
 
     if foundScript then
+        -- locals transfer over from the original main.lua script (which is sort of bad but shhh)
         local start = "local PKG_ROOT = \"" .. foundScript.Repo "\"\n"
         start = start .. "local PKG_NAME = \"" .. foundTarget.Parent .. "\"\n"
         start = start .. "local PATH = \"" .. foundScript.Path .. "\"\n"
-        start = start .. ("local __stdlibs = [=%s=[%s]=%s=]"):format("=", stdlibs, "=")
-
-        -- require resolve
-        start = start .. "local __scripts = {\n"
-
-        for k, v in pairs(__scripts) do
-            local name = k
-            if v.Parent == "" then
-                name = PKG_NAME .. v.__named
-            end
-            if v.Parent == foundTarget.Parent then
-                name = v.__named
-            end
-            start = start .. ("\t[\"%s\"] = { Source = [=[%s]=], __named = \"%s\", Path = \"%s\", Parent = \"%s\" },\n"):format(name, v.Source, v.__named, v.Path, v.Parent)
-        end
-
-        start = start .. "}\n\n"
-        start = start .. __stdlibs .. "\n"
 
         return loadstring(start .. foundScript.Source)()
     end
