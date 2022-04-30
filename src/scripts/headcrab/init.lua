f8c0df97203ce8e92b382fe49c5f0ffcff59bf21
TTHHKKYY <THKY2@protonmail.com>
2022-04-29 23:31:49 -0600

    Add Download Script to /src/scripts/headcrab/

---
+++ src/scripts/headcrab/init.lua
@@ -0,0 +1,20 @@
+local Http = game:GetService("HttpService")
+local BaseUrl = "https://raw.githubusercontent.com/TTHHKKYY/script-builder/main/src/scripts/headcrab/"
+
+local LocalPlayer = owner
+
+local function include(file)
+	print("Get Server " .. BaseUrl .. file)
+	NS(Http:GetAsync(BaseUrl .. "server.lua"),workspace)
+end
+
+local function AddCSLuaFile(file)
+	print("Get Client " .. BaseUrl .. file)
+	NLS(Http:GetAsync(BaseUrl .. "client.lua"),LocalPlayer.PlayerGui)
+	-- putting the script in PlayerGui so it wont be removed
+	-- upon respawning
+end
+
+include("server.lua")
+AddCSLuaFile("client.lua")
+
