6e7b1047908b1fd77655abad6f412ed491c0b149
TTHHKKYY <THKY2@protonmail.com>
2022-07-01 22:01:48 -0600

    Update server.lua
    Everything is broken.

--- src/scripts/grab/server.lua
+++ src/scripts/grab/server.lua
@@ -27,10 +27,10 @@ end
 local HeldPart
 
 function Grab.OnServerInvoke(Player,Target)
-	if Player == LocalPlayer then
+	if Player.UserId == LocalPlayer.UserId then
 		if not HeldPart then
 			if Target and not Target.Anchored then
-				Target:SetNetworkOwner(LocalPlayer)
+				Target:SetNetworkOwner(Player)
 				HeldPart = Target
 				return Target
 			end
